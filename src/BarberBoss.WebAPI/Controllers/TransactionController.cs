758888a988bc0cd4cd34434fb1c92cf889d45058
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2025-06-06 20:40:00 -0300

    feat: create structure for transaction endpoints

---
+++ src/BarberBoss.WebAPI/Controllers/TransactionController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarberBoss.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] object newTransaction)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    public IActionResult FindAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult FindOne([FromRoute] Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update([FromRoute] Guid id, [FromBody] object updatedTransaction)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("report/{type}")]
+    public IActionResult Report([FromRoute] string type)
+    {
+        throw new NotImplementedException();
+    }
+}
